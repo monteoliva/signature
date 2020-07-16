d8c34642e8c5722f4e2f4669d964b4310323c194
Claudio Monteoliva <claudio@monteoliva.net.br>
2020-07-16 15:04:25 -0300

    updated

--- app/src/main/java/br/com/jadlog/signature/view/BaseActivity.kt
+++ app/src/main/java/br/com/jadlog/signature/view/BaseActivity.kt
@@ -1,11 +1,12 @@
 package br.com.jadlog.signature.view
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 abstract class BaseActivity(resourceView: Int) : AppCompatActivity(resourceView) {
 
-    override fun onStart() {
-        super.onStart()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         initViews()
         initViewModel()
     }
